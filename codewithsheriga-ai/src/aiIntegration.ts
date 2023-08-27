acf1c5c67a32921d83a77bd55e82c3af3db915e7
Sherigabia <orionsavvy@gmail.com>
2023-08-27 13:24:08 +0000

    updated the apiUrl

--- codewithsheriga-ai/src/aiIntegration.ts
+++ codewithsheriga-ai/src/aiIntegration.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 async function getAIPoweredBotResponse(prompt: string): Promise<string>{
-    const apiUrl= "http://localhost:3000"
+    const apiUrl= "https://aicompanion-awhg.onrender.com"
 
     try{
         const response = await axios.post(apiUrl, { prompt })
